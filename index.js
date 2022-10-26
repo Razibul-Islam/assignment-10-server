1adfdaf5f58a574be7a243ed0d06dd8c703cd9d1
Razibul Islam <razibulislam665@gmail.com>
2022-10-26 22:06:52 +0600

    json file set

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 const port = 5000
 
-
+app.use(cors());
 
 const course = require('./data/course.json');
 
@@ -11,7 +11,9 @@ app.get('/', (req, res) => {
     res.send('Hello This is a server');
 })
 
-
+app.get('/course', (req, res) => {
+    res.send(course)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
